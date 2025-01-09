f5974a2b0e22cb366fa05b05045be513f23cceb1
Eliot Heinrich <eliot.heinrich@gmail.com>
2025-01-09 16:38:58 -0500

    Updated

--- pyqca/QCAConfig.py
+++ pyqca/QCAConfig.py
@@ -1,28 +1,46 @@
 import numpy as np
-from dataframe import Simulator, ExperimentParams
+from dataframe import Simulator, register_component
 from qutils import QuantumCHPState, QuantumCircuit, PauliString, EntropySampler
+
+
+def cluster_circuit(num_qubits):
+    circuit = QuantumCircuit(num_qubits)
+    for i in range(num_qubits):
+        circuit.h(i)
+
+    for i in range(num_qubits):
+        circuit.cz(i, (i + 1) % num_qubits)
+
+    return circuit
+
+def modified_cluster_circuit(num_qubits):
+    circuit = cluster_circuit(num_qubits)
+    for i in range(num_qubits):
+        circuit.cx(i, (i + 1) % num_qubits)
+    return circuit
+
 
 class QCASimulator(Simulator):
     def __init__(self, params, num_threads):
         super().__init__(params, num_threads)
 
         self.state = None
+        self.circuit = None
 
-        self.sampler = EntropySampler(params)
+        self.sampler = register_component(EntropySampler, params)
 
         self.system_size = params["system_size"]
         self.mzr_prob = params["mzr_prob"]
-
-        self.circuit = QuantumCircuit(self.system_size)
-        for i in range(self.system_size):
-            self.circuit.h(i)
-
-        for i in range(self.system_size):
-            self.circuit.cz(i, (i + 1) % self.system_size)
+        self.circuit_type = params["circuit_type"]
 
 
     def init(self, serialized_data=None):
         self.state = QuantumCHPState(self.system_size)
+
+        if self.circuit_type == "cluster":
+            self.circuit = cluster_circuit(self.system_size)
+        elif self.circuit_type == "modified_cluster":
+            self.circuit = modified_cluster_circuit(self.system_size)
 
     def timesteps(self, num_steps):
         for _ in range(num_steps):
@@ -42,12 +60,6 @@ class QCASimulator(Simulator):
 
         samples["entanglement"] = np.array([s]).T
 
-        #sites = list(range(self.system_size//2))
-
-        #s = self.state.entropy(sites, 2)
-
-        #samples["entanglement"] = [[s]]
-
         return samples
 
     def serialize(self):
@@ -56,5 +68,6 @@ class QCASimulator(Simulator):
     def get_texture(self):
         red = [1.0, 0.0, 0.0]
         blue = [0.0, 0.0, 1.0]
-        purple = [1.0, 0.0, 1.0]
+        purple = [0.0, 0.0, 1.0]
+        print(self.state)
         return self.state.get_texture(red, blue, purple)
